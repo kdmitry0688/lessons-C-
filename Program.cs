b6d222b3900e33c73f0b09e9a027396b211efef4
Dmitriy <kazany.dmitry@gmail.com>
2022-08-24 00:08:30 +0300

    add  Example 002 HelloUser

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿Console.WriteLine("Введите Ваше имя ");
 string username = Console.ReadLine();
-Console.Write("Пивет, ");
-Console.Write(username);
+Console.Write("Пивет,  ");
+Console.WriteLine(username);
